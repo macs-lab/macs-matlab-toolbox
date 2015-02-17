218fbaf156c303b36d21733a8d6631d39d09f2ec
xchenuconn <xchen@engineer.uconn.edu>
2015-02-17 14:58:06 -0500

    add function

---
+++ phase_rad2angle.m
@@ -0,0 +1,8 @@
+function pha = phase_rad2angle(pha_rad)
+% function pha = phase_rad2angle(pha_rad)
+% Translates a phase from rad to angle, and normalize the result to be
+% within plus minus 180 degrees.
+% 
+% Xu Chen
+% xchen@engr.uconn.edu
+pha = mod(pha_rad*180/pi+180,360)-180;
